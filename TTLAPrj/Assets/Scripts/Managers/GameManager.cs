dc35886a26cde8bdfdc6c7a3bca4f4f9c531ff56
ChanMin0334 <52943035+ChanMin0334@users.noreply.github.com>
2025-07-29 19:43:48 +0900

    [feat] Managers Base
    Manager 클래스들 싱글톤 화 및 대충 틀만 만들어 둠

--- TTLAPrj/Assets/Scripts/Managers/GameManager.cs
+++ TTLAPrj/Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 싱글톤 인스턴스
+    public static GameManager Instance { get; private set; }
+
+    // 게임 진입 했는지 여부
+    public bool isPlaying = false;
+
+    // 일시 정지 여부
+    // 레벨업,옵션 열기,게임 종료 시 사용
+    public bool isPaused = false;
+
+    private void Awake()
     {
-        
+        // 싱글톤 인스턴스 할당 및 중복 방지
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴 방지
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
+    // Start는 첫 프레임 업데이트 전에 호출됩니다.
+    void Start()
+    {
+        Init();
+    }
+
+    // Update는 매 프레임마다 호출됩니다.
     void Update()
     {
-        
+        if (isPlaying)
+        {
+            if (isPaused)
+            {
+                Time.timeScale = 0f; // 일시정지: 시간 멈춤
+            }
+            else
+            {
+                Time.timeScale = 1f; // 일시정지 해제: 시간 정상 진행
+            }
+        }
     }
+
+    private void Init()
+    {
+        throw new NotImplementedException();
+    }
+
 }
