a2affda528b6cd986f7d2fedb9a6acab356c3a5d
broccoli1962 <aq0011@naver.com>
2025-07-29 20:02:44 +0900

    [ feat ] JsonData Save and Load
    Unity JsonUtility를 이용하여 Save와 Load 시스템 임시 구현

--- TTLAPrj/Assets/Scripts/Managers/SaveManager.cs
+++ TTLAPrj/Assets/Scripts/Managers/SaveManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class SaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerTest
     {
-        
+        public float hp;
+        public float mp;
+        public string name;
+        public int gold;
+
+        public PlayerTest()
+        {
+            hp = 4f;
+            mp = 2f;
+            name = "broccoli";
+            gold = 10;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public PlayerTest playerData = new PlayerTest();
+
+    private string path;
+    [SerializeField] private string fileName = "save";
+
+    private void Awake()
     {
-        
+        path = Application.persistentDataPath + "/";
+        Debug.Log(path);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveData();
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            LoadData();
+        }
+    }
+
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(playerData);
+        File.WriteAllText(path + fileName, data);
+        Debug.Log("Data Save");
+    }
+
+    public void LoadData()
+    {
+        string data = File.ReadAllText(path + fileName);
+        playerData = JsonUtility.FromJson<PlayerTest>(data);
+        Debug.Log("Data Load"); 
     }
 }
