a1ee33362c048723daab4a136edaa521fb9426a7
SJL2002 <sungjunlim13@gmail.com>
2025-08-01 10:25:18 +0900

    [ Init ] AnimationManager

--- TTLAPrj/Assets/Scripts/Managers/AnimationManagers.cs
+++ TTLAPrj/Assets/Scripts/Managers/AnimationManagers.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class AnimationManagers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    [Header("Particle Systems")]
+    public ParticleSystem Death;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator component missing" + gameObject.name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMoveAnimation(bool isMoving)
     {
-        
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    public void PlayDeath()
+    {
+        Death.Play();
     }
 }
