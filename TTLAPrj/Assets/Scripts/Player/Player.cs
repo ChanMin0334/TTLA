ad795f547e51d213302b900894388ec1aafb068d
SJL2002 <sungjunlim13@gmail.com>
2025-08-04 19:42:51 +0900

    [ Modify ] Layers Level Prefab

--- TTLAPrj/Assets/Scripts/Player/Player.cs
+++ TTLAPrj/Assets/Scripts/Player/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 
 //using UnityEditor.Experimental.GraphView;
@@ -177,7 +178,9 @@ public class Player : Entity
 
         if (projectile != null)
         {
-            GameObject proj = Instantiate(projectile, bowTransform.position, Quaternion.identity);
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+            GameObject proj = Instantiate(projectile, bowTransform.position, rotation);
             proj.layer = LayerMask.NameToLayer("PlayerProjectile");
 
             Rigidbody2D projRb = proj.GetComponent<Rigidbody2D>();
